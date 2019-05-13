668b3993ad0d9e7597c81804f98bb3b5e938c723
uggoprince <uggoprince@gmail.com>
2019-05-13 13:00:40 +0100

    feature(mark as sold):design feature marks advert as sold [Finishes #165881171]

--- UI/js/main.js
+++ UI/js/main.js
@@ -13,7 +13,26 @@ function slideToggle() {
     }
 }
 
+const toggleSoldOnDashboard = () => { // marks your Ad Sold or not
+    const markSold = document.getElementsByClassName('markSold');
+    const soldMarker = document.getElementsByClassName('soldMarker');
+    for (let i = 0; i < markSold.length; i++) {
+        soldMarker[i].addEventListener('change', (event) => {
+            if(event.target.checked) {
+                markSold[i].style.background = 'green';
+                markSold[i].style.color = 'white';
+                markSold[i].innerHTML = 'Sold'
+            } else {
+                markSold[i].style.background = '#ddd';
+                markSold[i].style.color = 'black';
+                markSold[i].innerHTML = 'Mark as Sold'
+            }
+        });
+    }
+};
+
 document.addEventListener('DOMContentLoaded', ()=>{
     let nav_toggle = document.getElementById('nav-toggle');
     nav_toggle.addEventListener('click', slideToggle);
+    toggleSoldOnDashboard(); // marks your Ads Sold or not
 });
