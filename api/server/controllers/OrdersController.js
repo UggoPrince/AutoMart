cd5cb672b286da16362f051fd57f34cf8186d2e9
uggoprince <uggoprince@gmail.com>
2019-05-28 14:44:14 +0100

    feature(purchase order):update purchase order price endpoint [Finishes #166151975]

--- api/server/controllers/OrdersController.js
+++ api/server/controllers/OrdersController.js
@@ -24,11 +24,11 @@ class OrdersController {
       const idErrorMessages = [];
       if (!usersService.getAllUsers()[parseInt(buyer, 10) - 1]) { // -1 because it's an array
         idErrors = true;
-        idErrorMessages.push('Invalid buyer.');
+        idErrorMessages.push('Invalid buyer. No buyer with such id.');
       }
       if (!carsService.getAllCars()[parseInt(carId, 10) - 1]) { // -1 because it's an array
         idErrors = true;
-        idErrorMessages.push('Invalid carId');
+        idErrorMessages.push('Invalid carId. No car with such id.');
       }
       if (idErrors) {
         res.status(404).send({
@@ -42,6 +42,50 @@ class OrdersController {
         res.status(201).send({
           status: 201,
           data: madeOrder,
+        });
+      }
+    }
+  }
+
+  updateOrder(req, res) {
+    const amount = req.body.newAmount;
+    const orderId = req.params.order_id;
+    const validator = new Validator();
+    const validOrderUpdateReq = validator.validateUpdateOrderFields(amount, orderId);
+
+    if (validOrderUpdateReq.error) {
+      res.status(404).send({
+        status: 404,
+        error: validOrderUpdateReq.data,
+      });
+    } else if (!ordersService.getAllOrders()[orderId - 1]) { // -1 because it's an array
+      res.status(404).send({
+        status: 404,
+        error: 'Invalid orderId. No order with such id.',
+      });
+    } else {
+      const id = orderId - 1;
+      const getOrderStatus = ordersService.getOrderById(id).status;
+      let statusError = false;
+      let statusErrorMessages = '';
+      if (getOrderStatus === 'accepted') {
+        statusError = true;
+        statusErrorMessages = 'Your order has been accepted and can not be updated anymore';
+      } else if (getOrderStatus === 'rejected') {
+        statusError = true;
+        statusErrorMessages = 'Your order was rejected and can not be updated. Make a new order.';
+      }
+
+      if (statusError) {
+        res.status(404).send({
+          status: 404,
+          error: statusErrorMessages,
+        });
+      } else {
+        const update = ordersService.update(id, amount);
+        res.status(200).send({
+          status: 200,
+          data: update,
         });
       }
     }
