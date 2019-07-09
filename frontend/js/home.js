913b94f036276b2c4ce6c2c6bfbbd5030ba79f9e
uggoprince <uggoprince@gmail.com>
2019-07-09 01:54:45 +0100

    feature(order):make purchase order from home page [Finishes

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -5,11 +5,12 @@
 /* eslint-disable camelcase */
 
 
-const showMakeOfferModal = () => {
+const showMakeOfferModal = (car_id) => {
   const makeOfferModal = document.getElementById('makeOfferModal'); // Get the modal
   const nameOfOrderPersonInput = document.getElementById('nameOfOrderPerson');
   const { first_name, last_name } = JSON.parse(localStorage.getItem('autoMartUser'));
   nameOfOrderPersonInput.value = `${first_name} ${last_name}`;
+  document.getElementById('carIdForOffer').value = car_id;
   makeOfferModal.style.display = 'block';
 };
 
@@ -23,17 +24,137 @@ const showReportAdModal = () => {
 
 let Adverts = [];
 
+class AdvertGetter {
+  async getData(url) {
+    const { token } = JSON.parse(localStorage.getItem('autoMartUser'));
+    const cars = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authentication: `${token}`,
+        // 'Content-Type': 'application/json',
+      },
+    }).then(response => response.json())
+      .catch(error => ({ fetchError: error.message }));
+    return cars;
+  }
+
+  async getAllAdverts() {
+    const { is_admin } = JSON.parse(localStorage.getItem('autoMartUser'));
+    let url = 'https://automarter.herokuapp.com/api/v1/car?status=available';
+    if (is_admin) url = 'https://automarter.herokuapp.com/api/v1/car';
+    const cars = await this.getData(url)
+      .then(data => data)
+      .catch(error => error);
+    return cars;
+  }
+}
+
+class Order {
+  async postData(url, formData) {
+    const { token } = JSON.parse(localStorage.getItem('autoMartUser'));
+    const serverRes = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authentication: `${token}`,
+      },
+      body: JSON.stringify(formData),
+    }).then(response => response.json())
+      .catch(error => ({ fetchError: error.message }));
+    return serverRes;
+  }
+
+  buildOrderData(form) {
+    return { carId: form.carId.value, amount: form.amount.value };
+  }
+
+  getNotify() {
+    return document.getElementsByClassName('serverNotice')[0];
+  }
+
+  handleNetworkError() {
+    const notify = this.getNotify();
+    notify.style.color = 'red';
+    notify.style.background = 'rgb(255, 182, 182)';
+    notify.innerHTML = 'Not connecting. Check your network.';
+  }
+
+  displayError(error) {
+    const notify = this.getNotify();
+    notify.innerHTML = error.amount;
+  }
+
+  handleUserError(resultError) {
+    const notify = this.getNotify();
+    notify.style.color = 'red';
+    notify.style.background = 'rgb(255, 182, 182)';
+    if (resultError.status === 401) {
+      notify.innerHTML = 'Session expired, login to continue.';
+      setTimeout(() => {
+        signOut('signin.html'); // from main.js
+      }, 1500);
+    } else this.displayError(resultError.error);
+  }
+
+  closeModalAfterSuccess() {
+    setTimeout(() => {
+      const makeOfferModal = document.getElementById('makeOfferModal'); // Get the modal
+      makeOfferModal.style.display = 'none';
+      const notify = this.getNotify();
+      notify.style.display = 'none';
+      notify.innerHTML = '';
+    }, 1500);
+  }
+
+  handleSuccess() {
+    const notify = this.getNotify();
+    notify.innerHTML = 'Successfully Ordered.';
+  }
+
+  async makeOrder(event) {
+    event.preventDefault();
+    const notify = this.getNotify();
+    notify.style.display = 'inline-block';
+    notify.style.color = 'green';
+    notify.style.background = 'rgb(198, 240, 198)';
+    notify.innerHTML = 'Wait. Processing...';
+    const form = event.target;
+    const formData = this.buildOrderData(form);
+    const url = 'https://automarter.herokuapp.com/api/v1/order';
+    const result = await this.postData(url, formData)
+      .then(data => data)
+      .catch(error => error);
+    if (result.fetchError) {
+      this.handleNetworkError();
+    } else if (result.error && result.status !== 401) {
+      this.handleUserError(result);
+    } else if (result.status === 201) {
+      this.handleSuccess();
+    }
+  }
+}
+
 const initMakeOfferModal = () => {
   const makeOfferModal = document.getElementById('makeOfferModal'); // Get the modal
   const span = document.getElementsByClassName('close')[0]; // Get the <span> element that closes the modal
+  const order = new Order();
+  const notify = order.getNotify();
+
+  makeOfferModal.addEventListener('submit', async (event) => {
+    await order.makeOrder(event);
+  });
 
   span.addEventListener('click', () => { // When the user clicks on <span> (x), close the modal
     makeOfferModal.style.display = 'none';
+    notify.style.display = 'none';
+    notify.innerHTML = '';
   });
 
   window.addEventListener('click', (event) => { // When the user clicks anywhere outside of the modal, close it
     if (event.target === makeOfferModal) {
       makeOfferModal.style.display = 'none';
+      notify.style.display = 'none';
+      notify.innerHTML = '';
     }
   });
 };
@@ -51,29 +172,6 @@ const initReportModal = () => {
       reportFraudModal.style.display = 'none';
     }
   });
-};
-
-const getData = async (url) => {
-  const { token } = JSON.parse(localStorage.getItem('autoMartUser'));
-  const cars = await fetch(url, {
-    method: 'GET',
-    headers: {
-      Authentication: `${token}`,
-      // 'Content-Type': 'application/json',
-    },
-  }).then(response => response.json())
-    .catch(error => ({ fetchError: error.message }));
-  return cars;
-};
-
-const getAllAdverts = async () => {
-  const { is_admin } = JSON.parse(localStorage.getItem('autoMartUser'));
-  let url = 'https://automarter.herokuapp.com/api/v1/car?status=available';
-  if (is_admin) url = 'https://automarter.herokuapp.com/api/v1/car';
-  const cars = await getData(url)
-    .then(data => data)
-    .catch(error => error);
-  return cars;
 };
 
 const buildAdBox = (ad) => { // adBox builder
@@ -128,7 +226,7 @@ const buildAdBox = (ad) => { // adBox builder
   makeOfferButton.innerHTML = 'Make Offer';
   makeOfferButton.className = 'revealOfferModal';
   makeOfferButton.addEventListener('click', () => {
-    showMakeOfferModal();
+    showMakeOfferModal(ad.id);
   });
 
   const reportAdButton = document.createElement('button'); // update price button
@@ -167,7 +265,8 @@ const displayAdverts = () => {
 
 document.addEventListener('DOMContentLoaded', async () => {
   // get all ads
-  const Ads = await getAllAdverts();
+  const adsGetter = new AdvertGetter();
+  const Ads = await adsGetter.getAllAdverts();
   if (Ads.status === 401) {
     signOut('signin.html'); // from main.js
   }
